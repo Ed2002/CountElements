cacb1e86d04a099091d9c4b881a6e1b588948f3e
Ed2002 <ed49516@gmail.com>
2024-04-06 02:05:19 -0300

    feat(CountElements/Client): add connection pipe client way.

---
+++ CountElements/CountElements.Client/Connection.cs
@@ -0,0 +1,34 @@
+﻿using System.IO.Pipes;
+
+namespace CountElements.Client
+{
+    public class Connection
+    {
+        private string NamePipeline = string.Empty;
+        public Connection(string _namePipeline = ".myPipe") => NamePipeline = _namePipeline;
+
+        public async Task Start()
+        {
+            while (true)
+            {
+                using (NamedPipeClientStream PipeClient = new(".", NamePipeline, PipeDirection.InOut))
+                {
+                    await PipeClient.ConnectAsync();
+
+                    using (StreamReader reader = new(PipeClient))
+                    using (StreamWriter writer = new(PipeClient))
+                    {
+                        Console.Write("Set e mensagem to send to server: ");
+                        string? message = Console.ReadLine();
+
+                        await writer.WriteLineAsync(message);
+                        await writer.FlushAsync();
+
+                        string? response = await reader.ReadLineAsync();
+                        Console.WriteLine($"[SERVER]: {response}");
+                    }
+                }
+            }
+        }
+    }
+}
--- CountElements/CountElements.Client/Program.cs
+++ CountElements/CountElements.Client/Program.cs
@@ -1,2 +1,6 @@
-﻿Console.WriteLine("Hello, Client!");
+﻿using CountElements.Client;
+
+Console.WriteLine("Client Start Running");
+Connection Connection = new("CE.Pipe");
+await Connection.Start();
 Console.ReadKey();
