90f9de45a512cdb028fc1cf9087b20448c8fcedc
Ed2002 <ed49516@gmail.com>
2024-04-06 03:07:47 -0300

    refactor(CountElements/Client): alter class connection just to read from server.

--- CountElements/CountElements.Client/Connection.cs
+++ CountElements/CountElements.Client/Connection.cs
@@ -2,29 +2,19 @@
 
 namespace CountElements.Client
 {
-    public class Connection
+    public class Connection(string NamePipeline = ".myPipe")
     {
-        private string NamePipeline = string.Empty;
-        public Connection(string _namePipeline = ".myPipe") => NamePipeline = _namePipeline;
-
         public async Task Start()
         {
             while (true)
             {
-                using (NamedPipeClientStream PipeClient = new(".", NamePipeline, PipeDirection.InOut))
+                using (NamedPipeClientStream PipeClient = new(".", NamePipeline, PipeDirection.In))
                 {
                     await PipeClient.ConnectAsync();
 
                     using (StreamReader reader = new(PipeClient))
-                    using (StreamWriter writer = new(PipeClient))
                     {
-                        Console.Write("Set e mensagem to send to server: ");
-                        string? message = Console.ReadLine();
-
-                        await writer.WriteLineAsync(message);
-                        await writer.FlushAsync();
-
-                        string? response = await reader.ReadLineAsync();
+                        string? response = await reader.ReadToEndAsync();
                         Console.WriteLine($"[SERVER]: {response}");
                     }
                 }
